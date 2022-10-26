830cd78ae6747de958f758643c0c5f2578420149
gleonsrz <gonzalo15_115@hotmail.com>
2022-10-26 18:04:49 -0500

    1.0.22

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //public Transform camTarget;
-    //public float pLerp = .02f;
-    //public float rLerp = .01f;
+    public Transform camTarget;
+    public float pLerp = .02f;
+    public float rLerp = .01f;
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
-    //    transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, pLerp);
-    //}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, pLerp);
+    }
 }
---
+++ Assets/Scripts/Camera/ZoomWithScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomWithScroll : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 10f;
+
+    private Camera zoomCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoomCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(zoomCamera.orthographic)
+        {
+            zoomCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        } else
+        {
+            zoomCamera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        }
+
+    }
+}
